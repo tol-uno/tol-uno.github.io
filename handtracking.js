469239bc1afea9f8e74fdd6f484681a47fe1cfcc
tol-uno <unot46@gmail.com>
2023-01-24 20:14:14 -0500

    dot hands and event log

--- handtracking.js
+++ handtracking.js
@@ -1,19 +1,25 @@
 var colored_box;
 var text;
+var rightHand;
 
 function init(){
     colored_box = document.querySelector("#colored_box");
     text = document.querySelector("a-text");
+    rightHand = document.querySelector("#rightHand");
 
     console.log("logged: " + colored_box.getAttribute("color"));
     colored_box.setAttribute("color", "purple");
     console.log("logged: " + colored_box.getAttribute("color"));
     console.log("text: " + text.getAttribute("value"));
-    text.setAttribute("value", "js init ran");
+    text.setAttribute("value", "text show event");
+
+    rightHand.dispatchEvent("pinchstarted");
 }
+
+
 
 window.addEventListener("pinchstarted", function(e) {
     colored_box.setAttribute("color", "brown");
-    text.setAttribute("value", "die die die die die");
+    text.setAttribute("value", toString(e));
     console.log(e);
 })
