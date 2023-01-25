d0d4c9ac0a22f683f634e083fe3a43f0dde09347
tol-uno <unot46@gmail.com>
2023-01-24 19:39:00 -0500

    added debug text

--- handtracking.js
+++ handtracking.js
@@ -1,18 +1,15 @@
 function init(){
     var colored_box = document.querySelector("#colored_box");
+    var text = document.querySelector("a-text");
+
     console.log("logged: " + colored_box.getAttribute("color"));
     colored_box.setAttribute("color", "purple");
     console.log("logged: " + colored_box.getAttribute("color"));
-
+    console.log("text: " + text.getAttribute("value"));
+    text.setAttribute("value", "POOPY poopy");
 }
 
 window.addEventListener("pinchstarted", function(e) {
     colored_box.setAttribute("color", "green");
+    text.setAttribute("value",e)
 })
-
-// window.addEventListener("keydown", function(e) {
-//     myGameArea.keys[e.key] = true;
-// })
-// window.addEventListener("keyup", function(e) {
-//     myGameArea.keys[e.key] = false;
-// })
