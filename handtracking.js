360bb34a0d50d14dbbe486df4a654afcc02a9b79
tol-uno <unot46@gmail.com>
2023-01-25 16:27:33 -0500

    get id of event target

--- handtracking.js
+++ handtracking.js
@@ -1,29 +1,29 @@
 var colored_box;
-var text;
+var text1;
 var text2;
 var text3;
 var rightHand;
 
 function init(){
     colored_box = document.querySelector("#colored_box");
-    text = document.querySelector("#text1");
+    text1 = document.querySelector("#text1");
     text2 = document.querySelector("#text2");
     text3 = document.querySelector("#text3");
     rightHand = document.querySelector("#rightHand");
 
-    console.log("logged: " + colored_box.getAttribute("color"));
+    // console.log("logged: " + colored_box.getAttribute("color"));
     colored_box.setAttribute("color", "purple");
-    console.log("logged: " + colored_box.getAttribute("color"));
-    console.log("text: " + text.getAttribute("value"));
-    text.setAttribute("value", "text show event 2");
+    // console.log("logged: " + colored_box.getAttribute("color"));
+    // console.log("text: " + text1.getAttribute("value"));
+    text1.setAttribute("value", "text1 - getting id");
 }
 
 
 
 window.addEventListener("pinchstarted", function(e) {
     colored_box.setAttribute("color", "brown");
-    text.setAttribute("value", e);
-    text2.setAttribute("value", e.target);
+    text1.setAttribute("value", e);
+    text2.setAttribute("value", e.target.id);
     text3.setAttribute("value", toString(e.target));
     console.log(e);
 })
