8f7f9ca16f1b30400be6ec7707574566c919db18
tol-uno <unot46@gmail.com>
2023-01-24 20:44:58 -0500

    multi text

--- handtracking.js
+++ handtracking.js
@@ -1,25 +1,29 @@
 var colored_box;
 var text;
+var text2;
+var text3;
 var rightHand;
 
 function init(){
     colored_box = document.querySelector("#colored_box");
-    text = document.querySelector("a-text");
+    text = document.querySelector("#text1");
+    text2 = document.querySelector("#text2");
+    text3 = document.querySelector("#text3");
     rightHand = document.querySelector("#rightHand");
 
     console.log("logged: " + colored_box.getAttribute("color"));
     colored_box.setAttribute("color", "purple");
     console.log("logged: " + colored_box.getAttribute("color"));
     console.log("text: " + text.getAttribute("value"));
-    text.setAttribute("value", "text show event");
-
-    rightHand.dispatchEvent("pinchstarted");
+    text.setAttribute("value", "text show event 2");
 }
 
 
 
 window.addEventListener("pinchstarted", function(e) {
     colored_box.setAttribute("color", "brown");
-    text.setAttribute("value", toString(e));
+    text.setAttribute("value", e);
+    text2.setAttribute("value", e.target);
+    text3.setAttribute("value", toString(e.target));
     console.log(e);
 })
