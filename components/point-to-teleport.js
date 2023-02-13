7d50f4a969a43cc56f3c8a5513f3d9daf2955db9
tol-uno <unot46@gmail.com>
2023-02-13 18:50:54 -0500

    added testing telebutton

--- components/point-to-teleport.js
+++ components/point-to-teleport.js
@@ -5,42 +5,23 @@ AFRAME.registerComponent('point-to-teleport', {
     },
   
     init: function () {
+      
       // this.worldPosition = new THREE.Vector3();
       // this.handEls = document.querySelectorAll('[hand-tracking-controls]');
       // this.pressed = false;
 
-    //   this.el.emit('pressedended');
-    //   console.log("tried to emit pinchmoved");
-      this.el.cr
-      this.el.setAttribute("pressable", {pressDistance: "0.02"});
+      //   this.el.emit('pressedended');
+      //   console.log("tried to emit pinchmoved");
+      
+      var teleButton = document.createElement("a-entity");
+      this.el.appendChild(teleButton);
+      teleButton.setAttribute("pressable", {pressDistance: "0.02"});
+      teleButton.setAttribute("geometry", "primitive: sphere; radius: 0.04");
+      teleButton.setAttribute("position", "0 1.3 -1");
     },
   
     tick: function () { // every frame
-      var handEls = this.handEls;
-      var handEl;
-      var distance;
-      for (var i = 0; i < handEls.length; i++) { // loop through (2) hands-tracking-controls
-        handEl = handEls[i];
-        distance = this.calculateFingerDistance(handEl.components['hand-tracking-controls'].indexTipPosition);
-        if (distance < this.data.pressDistance) { //if within press distance
-          if (!this.pressed) { this.el.emit('pressedstarted'); }    // if not yet pressed. emit event
-          this.pressed = true;                                      // make sure pressed is true
-          return;                                                   // return exits whole function
-        }
-      }
-      if (this.pressed) { this.el.emit('pressedended'); }   // this only runs if return distance is too large for both hands
-      this.pressed = false;
     },
   
-    calculateFingerDistance: function (fingerPosition) { // gets distance between button and index finger
-      var el = this.el; // entity that has this component attached
-      var worldPosition = this.worldPosition; // position of button
-  
-      worldPosition.copy(el.object3D.position); //idk whats really goin on here. Some threejs stuff
-      el.object3D.parent.updateMatrixWorld();
-      el.object3D.parent.localToWorld(worldPosition);
-  
-      return worldPosition.distanceTo(fingerPosition);
-    }
   });
   
