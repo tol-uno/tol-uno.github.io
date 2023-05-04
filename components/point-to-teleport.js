49ea3dfe0a204c2fd48d04d8221f92b2de22772b
tol-uno <unot46@gmail.com>
2023-05-04 08:23:28 -0400

    remove extra stuff and add henry

--- components/point-to-teleport.js
+++ components/point-to-teleport.js
@@ -11,38 +11,38 @@ AFRAME.registerComponent('point-to-teleport', {
       
       console.log(handEl.components['hand-tracking-controls']);
 
+
+      // CREATE BUTTON ENTITY
       var teleButton = document.createElement("a-entity");
       teleButton.setAttribute("id", "teleButton");
-
-      this.el.appendChild(teleButton);
       teleButton.setAttribute("pressable", {pressDistance: "0.02"});
       teleButton.setAttribute("geometry", "primitive: sphere; radius: 0.02");
       teleButton.setAttribute("position", "0 1.3 -1");
+      this.el.appendChild(teleButton);
+
       this.el.addEventListener("pressedstarted", function(e) {
         if (e.target == teleButton) {
-          // teleButton.setAttribute("position", "0 2 -1");
 
+          // MOVE TELE BUTTON TO FINGER
           var fingerx = handEl.components['hand-tracking-controls'].indexTipPosition.x
           var fingery = handEl.components['hand-tracking-controls'].indexTipPosition.y
           var fingerz = handEl.components['hand-tracking-controls'].indexTipPosition.z
-
           teleButton.setAttribute("position", { x: fingerx , y: fingery , z: fingerz});
 
-          var jointsArray = handEl.components['hand-tracking-controls'].jointEls;
 
-          // document.querySelector("#text3").setAttribute("value", "x: " + fingerx + " y: " + fingery+ " z: " + fingerz);
-          document.querySelector("#text1").setAttribute("value", handEl.components['hand-tracking-controls'].jointEls[wrist]);
-          document.querySelector("#text2").setAttribute("value", jointsArray[0].keys(jointsArray[0]));
-          document.querySelector("#text3").setAttribute("value", jointsArray);
-          
+          // DEBUG TEXT TRYING TO ACCESS JOINTS
+          var jointsArray = handEl.components['hand-tracking-controls'].jointEls;
+          document.querySelector("#text1").setAttribute("value", handEl.components['hand-tracking-controls'].jointEls[0]);
+          document.querySelector("#text2").setAttribute("value", document.querySelector("#dom-xrhandjoint-wrist"));
+          // document.querySelector("#text3").setAttribute("value", jointsArray); 
 
         
-          // handEl.object3D.add(teleButton.object3D);
-          }
-        })
+        }
+      })
     },
 
     tick: function () { // every frame
+    
     },    
 
   });
